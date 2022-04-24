91a6d216f46547ac95b6165231cfffa39b6a598c
Pavel <paul@paulrz.cz>
2022-04-25 00:42:29 +0300

    AutoTrade urls

--- autotrade.go
+++ autotrade.go
@@ -1,5 +1,12 @@
 package c2api
 
+const (
+	GetListAvailableBrokersUrl string = "getListAvailableBrokers"
+	ChangeAutoTradeUrl         string = "changeAutoTrade"
+	StopAutoTradeUrl           string = "stopAutoTrade"
+	StartAutoTradeUrl          string = "startAutoTrade"
+)
+
 type AutoTradeManagementService struct {
 	client *Client
 }
